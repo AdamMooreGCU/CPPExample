7b1d707d803c0c963b0c1e593cc091cdb43ccfcf
AdamMooreGCU <Adam.Moore1@my.gcu.edu>
2026-02-18 11:34:07 -0700

    Add project files.

---
+++ Source/CPPExample/CPPExample.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CPPExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPPExample, "CPPExample" );
